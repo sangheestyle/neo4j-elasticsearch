b8715d8342a79c6ba03f1a3fae70c069bd5f1ddc
Sanghee Kim <sangheestyle@gmail.com>
2015-10-02 15:11:51 -0400

    DRAFT: Importing relationships from Neo4j to ElasticSearch
    WARNING: This code should be extremly ugly. Don't read it.
    
    The original plug-in can import only nodes from Neo4j to ElasticSearch.
    What if we want to import relationships also? Here is the simple and
    ugly answer.
    
    However, it's not the correct answer yet. Just importing ugly.
    
    TODO: Make it more beauty

--- src/main/java/org/neo4j/elasticsearch/ElasticSearchEventHandler.java
+++ src/main/java/org/neo4j/elasticsearch/ElasticSearchEventHandler.java
@@ -47,6 +47,7 @@ class ElasticSearchEventHandler implements TransactionEventHandler<Collection<Bu
         //List<BulkableAction> actions = new ArrayList<>(1000);
         Map<IndexId, BulkableAction> actions = new HashMap<>(1000);
         for (Node node : transactionData.createdNodes()) {
+            System.out.println(node.getProperty("name"));
             if (hasLabel(node)) actions.putAll(indexRequests(node));
         }
         for (Node node : transactionData.deletedNodes()) {
@@ -65,6 +66,11 @@ class ElasticSearchEventHandler implements TransactionEventHandler<Collection<Bu
         for (PropertyEntry<Node> propEntry : transactionData.removedNodeProperties()) {
             if (hasLabel(propEntry))
                 actions.putAll(updateRequests(propEntry.entity()));
+        }
+
+        for (Relationship relationship : transactionData.createdRelationships()) {
+            System.out.println(relationship.getProperty("where"));
+            actions.putAll(indexRequests(relationship));
         }
         return actions.isEmpty() ? Collections.<BulkableAction>emptyList() : actions.values();
     }
@@ -112,6 +118,7 @@ class ElasticSearchEventHandler implements TransactionEventHandler<Collection<Bu
             if (!indexLabels.contains(l)) continue;
 
             for (ElasticSearchIndexSpec spec: indexSpecs.get(l)) {
+                System.out.println("aaa: " + spec.getProperties());
                 String id = id(node), indexName = spec.getIndexName();
                 reqs.put(new IndexId(indexName, id), new Index.Builder(nodeToJson(node, spec.getProperties()))
                 .type(l.name())
@@ -120,6 +127,19 @@ class ElasticSearchEventHandler implements TransactionEventHandler<Collection<Bu
                 .build());
             }
         }
+        return reqs;
+    }
+
+    private Map<IndexId, Index> indexRequests(Relationship relationship) {
+        HashMap<IndexId, Index> reqs = new HashMap<>();
+        String id = id(relationship), indexName = "relationships";
+        Set<String> properties = new HashSet<String>(Arrays.asList("where"));
+        reqs.put(new IndexId(indexName, id), new Index.Builder(relationshipToJson(relationship, properties))
+                .type("relationship")
+                .index(indexName)
+                .id(id)
+                .build());
+
         return reqs;
     }
 
@@ -177,12 +197,27 @@ class ElasticSearchEventHandler implements TransactionEventHandler<Collection<Bu
         return String.valueOf(node.getId());
     }
 
+    private String id(Relationship relationship) {
+        return String.valueOf(relationship.getId());
+    }
+
     private Map nodeToJson(Node node, Set<String> properties) {
         Map<String,Object> json = new LinkedHashMap<>();
         json.put("id", id(node));
         json.put("labels", labels(node));
         for (String prop : properties) {
             Object value = node.getProperty(prop);
+            json.put(prop, value);
+        }
+        return json;
+    }
+
+    private Map relationshipToJson(Relationship relationship, Set<String> properties) {
+        Map<String,Object> json = new LinkedHashMap<>();
+        json.put("id", id(relationship));
+        json.put("labels", "relationship");
+        for (String prop : properties) {
+            Object value = relationship.getProperty(prop);
             json.put(prop, value);
         }
         return json;
